f98d9fbc17d78b06c704c8576d856835bd10b1ed
denisshav <denis.shavshin@gmail.com>
2020-07-10 08:37:05 +0300

    Add docs

---
+++ docs/precache-manifest.073180ec99c99c22f7e3221e38ed57a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76128a2311bbfb5cea97f9f28d28f3c5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0090d20ac17308a98113",
+    "url": "./static/css/2.3c515868.chunk.css"
+  },
+  {
+    "revision": "5ca35c53f86c228d476c",
+    "url": "./static/css/main.95c8ba52.chunk.css"
+  },
+  {
+    "revision": "0090d20ac17308a98113",
+    "url": "./static/js/2.35b725b1.chunk.js"
+  },
+  {
+    "revision": "21974dbef75b80615a91ee5e1c84fd76",
+    "url": "./static/js/2.35b725b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ca35c53f86c228d476c",
+    "url": "./static/js/main.d3a9b3e5.chunk.js"
+  },
+  {
+    "revision": "0aa7c98e0e68f9552f35",
+    "url": "./static/js/runtime-main.07e03aea.js"
+  },
+  {
+    "revision": "01327c907fafb23d4ed72b68c45d2bef",
+    "url": "./static/media/profile-img.01327c90.png"
+  }
+]);
