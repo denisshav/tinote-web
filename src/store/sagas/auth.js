87ac561026c135950bb12cc929e040cece43ca55
denisshav <denis.shavshin@gmail.com>
2020-07-15 17:23:13 +0300

    Implement check token on init app

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -1,43 +1,23 @@
 import { put, call } from "redux-saga/effects"
 import * as actions from "../actions/index"
-import auth from "../../myAuth"
 import axios from "../../axios-server"
-
-// export function* logoutSaga(action) {
-//   yield call([localStorage, "removeItem"], "token")
-//   yield put(actions.authStart())
-
-//   try {
-//     auth.signOut()
-//     yield put(actions.logoutSucceed())
-//   } catch (error) {
-//     yield put(actions.authFail(error))
-//   }
-// }
 
 export function* logoutSaga(action) {
   yield call([localStorage, "removeItem"], "token")
-  // yield put(actions.authStart())
-
-  // try {
-  auth.signOut()
   yield put(actions.logoutSucceed())
-  // } catch (error) {
-    // yield put(actions.authFail(error))
-  // }
 }
 
 export function* signInSaga(action) {
   yield put(actions.authStart())
-  console.log(action.email)
+
   const user = {
     email: action.email,
-    password: action.password
+    password: action.password,
   }
-  
+
   try {
     const response = yield axios.post("/user/login", user)
-    console.log(response.headers)
+
     localStorage.setItem("token", response.headers["auth-token"])
     yield put(actions.authSuccess(response.headers["auth-token"]))
   } catch (error) {
@@ -51,12 +31,12 @@ export function* signUpSaga(action) {
 
   const user = {
     email: action.email,
-    password: action.password
+    password: action.password,
   }
 
   try {
     const response = yield axios.post("/user/register", user)
-    console.log(response.headers)
+
     localStorage.setItem("token", response.headers["auth-token"])
     yield put(actions.authSuccess(response.headers["auth-token"]))
   } catch (error) {
@@ -66,21 +46,12 @@ export function* signUpSaga(action) {
 }
 
 export function* checkAuthStateSaga(action) {
-  // yield put(actions.authStart())
+  yield put(actions.authStart())
 
-  // try {
-  //   yield new Promise((resolve, reject) => {
-  //     auth.onAuthStateChanged(user => {
-  //       if (user) {
-  //         resolve()
-  //       } else {
-  //         reject()
-  //       }
-  //     })
-  //   })
-
-  //   yield put(actions.authSuccess())
-  // } catch {
-    yield put(actions.logoutSucceed())
-  // }
+  try {
+    yield axios.post("/user/verify?auth=" + localStorage.getItem("token"))
+    yield put(actions.authSuccess(localStorage.getItem("token")))
+  } catch (error) {
+    yield put(actions.logout())
+  }
 }
